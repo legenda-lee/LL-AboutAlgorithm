a231e24d6c645c3f8adc00d22b26929463b83e25
Legenda-Lee <lee.legenda@gmail.com>
2019-11-16 21:24:11 +0800

    init

---
+++ src/main/java/com/legenda/lee/algorithmcollection/AlgorithmCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.legenda.lee.algorithmcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legenda/lee/algorithmcollection/AlgorithmCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legenda.lee.algorithmcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
